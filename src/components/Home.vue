d03d506c558c04c69be48d037b31dc2a02334216
Jereme Allen <jereme.allen@wheniwork.com>
2017-03-31 16:09:30 -0500

    Add in home component

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class='col'>
+      Home
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Defense from '@/components/Defense';
+import Home from '@/components/Home';
 import Goalie from '@/components/Goalie';
 import Offense from '@/components/Offense';
 
@@ -8,6 +9,11 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
     {
       path: '/offense',
       name: 'offense',
